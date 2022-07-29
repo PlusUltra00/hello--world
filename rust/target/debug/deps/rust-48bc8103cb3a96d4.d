b8f7ce9ee50fe51e9a57f06139c69dde9b77ee29
GoldenRecord <89250932+PlusUltra00@users.noreply.github.com>
2022-07-29 22:42:53 +0900

    new

---
+++ rust/target/debug/deps/rust-48bc8103cb3a96d4.d
@@ -0,0 +1,7 @@
+c:\dev\hello, world\rust\target\debug\deps\rust-48bc8103cb3a96d4.rmeta: src\main.rs
+
+c:\dev\hello, world\rust\target\debug\deps\rust-48bc8103cb3a96d4.d: src\main.rs
+
+src\main.rs:
+
+# env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::needless_return__CLIPPY_HACKERY__
---
+++ rust/target/debug/deps/rust-f138a41f864881da.d
@@ -0,0 +1,7 @@
+c:\dev\hello, world\rust\target\debug\deps\rust-f138a41f864881da.rmeta: src\main.rs
+
+c:\dev\hello, world\rust\target\debug\deps\rust-f138a41f864881da.d: src\main.rs
+
+src\main.rs:
+
+# env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::needless_return__CLIPPY_HACKERY__
